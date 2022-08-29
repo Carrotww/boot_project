5cde2578da7fc65594d787a3f050b1385c371e64
Carrot_hyeong <104682050+Carrotww@users.noreply.github.com>
2022-08-29 23:30:15 +0900

    commit

--- team_intro/Minipro_app.py
+++ team_intro/Minipro_app.py
@@ -1,20 +1,24 @@
-import re
+from flask import Flask, render_template, request, jsonify
+import re # 정규표현식 regex import
 from pymongo import MongoClient
+import requests
 client = MongoClient('mongodb+srv://root:1860@cluster0.8fpka1h.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-# 입력값 체크
+# 입력값 체크 정규표현식
 ch_name = re.compile('^[가-힣a-zA-Z]{2,5}$')
 ch_age = re.compile('^[0-9]+$')
 ch_mbti = re.compile('^[a-zA-Z]+$')
 ch_blog = re.compile('^(((http(s?))\:\/\/)?)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$')
 
+app = Flask(__name__)
+
 # Team project
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('main.html')
 
-def input_check(doc: dict):
+def input_check(doc: dict): # dict() 형식으로 받아 입력값 체크 함수 호출
     if not ch_name.match(doc['name']):
         return jsonify({'msg':'이름을 확인해 주세요'})
     if not ch_age.match(doc['age']):
@@ -24,10 +28,22 @@ def input_check(doc: dict):
     if not ch_blog.match(doc['blog']):
         jsonify({'msg': '블로그 형식을 확인해 주세요'})
 
-@app.route("/index/sign", methods=["POST"])
-def team_post():
-    name_receive = request.form['name']
-    comment_receive = request.form['comment_give']
+@app.route("/team")
+def show_team_list():
+    return render_template('team.html')
+
+@app.route("/team/post", methods=["POST"])
+def team_post(): # 팀원 등록하기
+    name_receive = request.form['name_give']
+    age_receive = request.form['age_give']
+    address_receive = request.form['address_give']
+    hobby_receive = request.form['hobby_give']
+    MBTI_receive = request.form['MBTI_give']
+    spec_receive = request.form['spec_give']
+    style_receive = request.form['style_give']
+    blog_receive = request.form['blog_give']
+    # img_url_receive = request.form['img_url_give']
+
 
     doc = {
         "name": name_receive,
@@ -36,7 +52,7 @@ def team_post():
         "hobby": hobby_receive,
         "MBTI": MBTI_receive,
         "spec": spec_receive,
-        "sytle": style_receive,
+        "style": style_receive,
         "blog": blog_receive,
         "img_url": img_url_receive
     }
@@ -53,10 +69,10 @@ def team_post():
         db.minproject.insert_one(doc)
         return jsonify({'msg':'팀원 추가 완료!'})
 
-@app.route("/index/sign", methods=["GET"])
-def team_get():
-    comment_list = list(db.homework.find({}, {'_id': False}))
-    return jsonify({'comments':comment_list})
+@app.route("/sign", methods=["GET"])
+def sign_get(): # 팀원 소개
+    # comment_list = list(db.minproject.find({}, {'_id': False}))
+    return render_template('sign.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
--- test2.py
+++ test2.py
@@ -1,6 +1,10 @@
 import re
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://root:1860@cluster0.8fpka1h.mongodb.net/?retryWrites=true&w=majority')
+import certifi
+ca = certifi.where()
+# client = MongoClient('mongodb+srv://root:1860@cluster0.8fpka1h.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:qmzp9128@cluster0.8uddjgf.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+# client = MongoClient('mongodb+srv://test:qmzp9128@cluster0.8uddjgf.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 ch_name = re.compile('^[가-힣a-zA-Z]{2,5}$')
@@ -8,8 +12,9 @@ ch_age = re.compile('^[0-9]+$')
 ch_mbti = re.compile('^[a-zA-Z]+$')
 ch_blog = re.compile('^(((http(s?))\:\/\/)?)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$')
 
+name_re = '유형석'
 doc = {
-    "name": '유형석',
+    "name": 'hyeongseok',
     "age": 27,
     "address": '일산',
     "hobby": '축구',
@@ -20,9 +25,11 @@ doc = {
     "img_url": '??'
 }
 
-test = ch_name.match('유형석sdf')
+db.minproject.insert_one(doc)
 
-if not ch_name.match('유형석sdfsf'):
+if not ch_name.match('유형석'):
     print('XX')
 else:
     print('OO')
+
+print(doc['name'])
